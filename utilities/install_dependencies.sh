8c2653066804108fe9ae366c3cff9163a7fcb124
Hugo Locurcio <hugo.locurcio@hugo.pro>
2017-12-19 13:29:22 +0100

    Make installing dependencies quieter

--- utilities/install_dependencies.sh
+++ utilities/install_dependencies.sh
@@ -9,12 +9,12 @@
 set -euo pipefail
 
 # Path to the Xcode DMG image
-export XCODE_DMG_PATH="$DIR/Xcode_7.3.1.dmg"
+export XCODE_DMG="$DIR/Xcode_7.3.1.dmg"
 
 echo -e "\n\e[4mInstalling dependencies (administrative privileges may be required)\e[0m\n"
 
 # Display a warning message if no Xcode DMG is found
-if [ ! -f "$XCODE_DMG_PATH" ]; then
+if [ ! -f "$XCODE_DMG" ]; then
   echo -e "\e[1;33mNOTE:\e[0m Couldn't find a Xcode 7.3.1 DMG image.\nIf you want to build for macOS and iOS, download it from here (requires a free Apple Developer ID):\n\e[1mhttps://developer.apple.com/download/more/\e[0m\n"
 fi
 
@@ -23,11 +23,12 @@ fi
 if [ -f "/etc/redhat-release" ]; then
   # Fedora
   sudo dnf update -y
-  sudo dnf install -y git cmake scons pkgconfig gcc-c++ curl libX11-devel libXcursor-devel \
-                      libXrandr-devel libXinerama-devel mesa-libGL-devel \
+  sudo dnf install -y git cmake scons pkgconfig gcc-c++ curl libxml2-devel libX11-devel \
+                      libXcursor-devel libXrandr-devel libXinerama-devel mesa-libGL-devel \
                       alsa-lib-devel pulseaudio-libs-devel freetype-devel \
                       openssl-devel libudev-devel mesa-libGLU-devel mingw32-gcc-c++ mingw64-gcc-c++ \
-                      mingw32-winpthreads-static mingw64-winpthreads-static wine
+                      mingw32-winpthreads-static mingw64-winpthreads-static wine \
+                      llvm-devel uuid-devel xar-devel fuse-devel
 else
   # Ubuntu
   sudo apt-get update -y
@@ -41,39 +42,39 @@ mkdir -p "$TOOLS_DIR"
 
 # Install InnoSetup
 
-if [ ! -d "$TOOLS_DIR/innosetup" ]; then
+if [  -d "$TOOLS_DIR/innosetup" ]; then
   curl -o "$TOOLS_DIR/innosetup.zip" "https://archive.hugo.pro/.public/godot-builds/innosetup-5.5.9-unicode.zip"
-  unzip "$TOOLS_DIR/innosetup.zip" -d "$TOOLS_DIR"
+  unzip -q "$TOOLS_DIR/innosetup.zip" -d "$TOOLS_DIR"
   rm "$TOOLS_DIR/innosetup.zip"
 fi
 
 # Install the Android SDK
 
-if [ ! -d "$TOOLS_DIR/android" ]; then
+if [  -d "$TOOLS_DIR/android" ]; then
   # Download and extract the SDK
   curl -o "$TOOLS_DIR/android.zip" "https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip"
   # The SDK tools must be located in `$TOOLS_DIR/android/tools` as
   # other directories will exist within `$TOOLS_DIR/android`
   mkdir "$TOOLS_DIR/android"
-  unzip "$TOOLS_DIR/android.zip" -d "$TOOLS_DIR/android"
+  unzip -q "$TOOLS_DIR/android.zip" -d "$TOOLS_DIR/android"
   rm "$TOOLS_DIR/android.zip"
 fi
 
-# If the user provides an Xcode DMG image, install OSXCross and darling-dmg
+# If the user provides an Xcode DMG image, install OSXCross
+# (which includes darling-dmg)
 
-if [ -f "$XCODE_DMG_PATH" ]; then
+if [ -f "$XCODE_DMG" ] && [ ! -d "$TOOLS_DIR/osxcross" ] && [ ! -d "$TOOLS_DIR/cctools-port" ]; then
   # OSXCross (for macOS builds)
-  curl -o "$TOOLS_DIR/osxcross.zip" "https://github.com/tpoechtrager/osxcross/archive/master.zip"
-  unzip "$TOOLS_DIR/osxcross.zip" -d "$TOOLS_DIR"
+  curl -o "$TOOLS_DIR/osxcross.zip" "https://codeload.github.com/tpoechtrager/osxcross/zip/master"
+  unzip -q "$TOOLS_DIR/osxcross.zip" -d "$TOOLS_DIR"
   mv "$TOOLS_DIR/osxcross-master" "$TOOLS_DIR/osxcross"
-
-  # darling-dmg (for iOS builds)
-  curl -o "$TOOLS_DIR/darling-dmg.zip" "https://github.com/darlinghq/darling-dmg/archive/master.zip"
-  unzip "$TOOLS_DIR/darling-dmg.zip" -d "$TOOLS_DIR"
-  mv "$TOOLS_DIR/darling-dmg-master" "$TOOLS_DIR/darling-dmg"
+  cd "$TOOLS_DIR/osxcross"
+  tools/gen_sdk_package_darling_dmg.sh "$XCODE_DMG"
+  mv "*.tar.xz" "$TOOLS_DIR/osxcross/tarballs"
+  UNATTENDED=1 ./build.sh
 
   # cctools-port (for iOS builds)
   curl -o "$TOOLS_DIR/cctools-port.zip" "https://github.com/tpoechtrager/cctools-port/archive/master.zip"
-  unzip "$TOOLS_DIR/cctools-port.zip" -d "$TOOLS_DIR"
+  unzip -q "$TOOLS_DIR/cctools-port.zip" -d "$TOOLS_DIR"
   mv "TOOLS_DIR/cctools-port-master" "$TOOLS_DIR/cctools-port"
 fi
